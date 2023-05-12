c9d64e5a1418fc9e54ca0665a750c619c9fd11e1
robeen <robeen.smiles@gmail.com>
2023-05-12 10:09:01 +0545

    ..

--- realtime_chat/asgi.py
+++ realtime_chat/asgi.py
@@ -2,7 +2,7 @@ import os
 
 from django.core.asgi import get_asgi_application
 django_asgi_app = get_asgi_application()
-#for channels####
+#for channels####.
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 import rtc_app.routing
