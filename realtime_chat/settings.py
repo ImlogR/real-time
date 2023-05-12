6e7ab9e405d4b3288a65814834192df04259e4a9
robeen <robeen.smiles@gmail.com>
2023-05-12 06:42:48 +0545

    ..

--- realtime_chat/settings.py
+++ realtime_chat/settings.py
@@ -173,10 +173,10 @@ USE_TZ = True
 
 # STATIC_URL = 'static/'
 STATIC_URL = 'static/'
-STATICFILES_DIRS= [
-    BASE_DIR / 'staticfiles'
-    ]
-# STATIC_ROOT= 'staticfiles/'
+# STATICFILES_DIRS= [
+#     BASE_DIR / 'staticfiles'
+#     ]
+STATIC_ROOT= '/var/www/real-time/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
