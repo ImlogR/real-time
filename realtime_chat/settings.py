d790e12b18dbaa63d9d952b346bf7cf1a59e94db
Imlog <robeen.smiles@gmail.com>
2023-05-31 14:25:15 +0545

    ..

--- realtime_chat/settings.py
+++ realtime_chat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-k*2fm^qhmzgyq$0&^2l&+axn-@l0nai$ji@kx%fysbdvoxy8sm
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
 
